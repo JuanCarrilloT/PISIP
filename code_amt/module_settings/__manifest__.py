9f7ac6a2dff7be74b3ba9031fb8a5022c7fd6d44
Jefferson16Jb <jb1993jeff@gmail.com>
2018-02-05 22:36:31 -0500

    Agregar codigo

---
+++ code_amt/module_settings/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name' : 'Módulo Seguridad y Configuración',
+    'version' : '1.0',
+    'summary': 'Módulo de seguridad y configuración',
+    'sequence': 1,
+    'description': """
+        Módulo administra la seguridad
+    """,
+    'category': 'Vehiculos',
+    'website': 'https://www.amt.com',
+    'images' : [],
+    'depends' : ['base'],
+    'data': [
+        'data/res_groups.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
